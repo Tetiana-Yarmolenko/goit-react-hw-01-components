daa74701575e5e68bb57c4890b0c7eee454406b6
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2021-01-08 23:05:32 +0200

    hw-1

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,39 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import s from "./Statistics.module.css";
-const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
-];
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-// function colorBgChange() {
-//   const colorIndex = randomIntegerFromInterval(0, colors.length - 1);
-//   const backgroundColor = colors[colorIndex];
-// }
 
-// function bodyColorChange() {
-//   if (isActive) { return; }
-//   isActive = true;
-//   timerId = setInterval(() => colorBgChange (), 1000);
 
-// }
+
 
 const StatsListItem = ({ id, label, percentage }) => {
-  const colorIndex = randomIntegerFromInterval(0, colors.length - 1);
-  const backgroundColorList = colors[colorIndex];
-  console.log(backgroundColorList);
   return (
     <li
       key={id}
-      className={s.item}
-      styles={{ backgroundColor: { backgroundColorList } }}
-    >
+      className={s.item}>
       <span className={s.label}>{label}</span>
       <span className={s.percentage}>{percentage}%</span>
     </li>
@@ -56,7 +32,7 @@ const Statistics = ({ items }) => {
   );
 };
 
-StatsList.propTypes = {
+Statistics.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
